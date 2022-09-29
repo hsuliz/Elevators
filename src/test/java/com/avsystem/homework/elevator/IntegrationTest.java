0b49d38ab68fd12f96cbfed771996cb786ce6155
mymytre <sasha.suliz@gmail.com>
2022-09-29 12:55:15 +0200

    Tests

--- src/test/java/com/avsystem/homework/elevator/IntegrationTest.java
+++ src/test/java/com/avsystem/homework/elevator/IntegrationTest.java
@@ -8,6 +8,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.List;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 @ActiveProfiles("test")
@@ -26,17 +28,30 @@ public class IntegrationTest {
 
     @Test
     void fullTest() {
+
+        var exepted = List.of();
+
         webClient.put().uri("/api/elevator")
                 .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue("{\"id\": 1, \"currentFlor\": 50}")
+                .bodyValue("{\"id\": 1, \"currentFlor\": 5}")
+                .exchange();
+
+        webClient.put().uri("/api/elevator")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue("{\"id\": 2, \"currentFlor\": 20}")
                 .exchange();
 
         System.out.println(elevatorList.getList());
+
         webClient.post().uri("/api/pick/10")
                 .exchange();
 
         webClient.post().uri("/api/step")
                 .exchange();
+
+        webClient.get().uri("/api/elevators")
+                .exchange()
+                .expectStatus().isEqualTo(2);
 
         System.out.println(elevatorList.getList());
     }
